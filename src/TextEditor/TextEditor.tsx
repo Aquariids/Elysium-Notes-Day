72c237b7b3dccc819c9f1287a362c23949af373d
Dima-kun <FaboritePonyville@gmail>
2023-03-09 22:10:58 +0400

    Preparation

--- src/TextEditor/TextEditor.tsx
+++ src/TextEditor/TextEditor.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 import ITextEditorProps from './TextEditor.props'
 import s from './TextEditor.module.scss';
 const TextEditor: React.FunctionComponent<ITextEditorProps> = (props) => {
+    let className = s.option_button + ' ' + s.format;
+
   return (
 
     <div className={s.container}>
         <div className={s.options}>
-            <button id="bold" className={s.option_button }></button>
+            <button id={s.bold} className={ className }>
+            <i className="fa-solid fa-bold"></i>
+            </button>
         </div>
     </div>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
     return(
       <Html lang='ru'>
           <Head>
-            <meta></meta>
+          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css'></link>
           </Head>
           <body>
             <Main/>
