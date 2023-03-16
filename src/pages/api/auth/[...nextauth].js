de86f8bbfd0d3240b464f95e998e425b06e4a263
Dima kun <FavoritePonyville@gmail.com>
2023-03-16 19:11:11 +0300

    test nextAuth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+
+import NextAuth from "next-auth";
+import GithubProvider  from "next-auth/providers/github";
+
+    const authOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        })
+                // Providers.Email({
+                //     server: {
+                //         host: "",
+                //         port: "",
+                //         auth: {
+                //             user: "",
+                //             pass: ""
+                //         }
+
+                //     },
+                //     from: "",
+                // })
+    ],
+
+    
+}
+export default (req,res) => NextAuth(req,res,authOptions)
