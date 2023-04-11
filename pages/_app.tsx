2effdbfb001221d3b8a7fdb48ae522215b8de819
Dima kun <FavoritePonyville@gmail.com>
2023-04-11 20:48:31 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
-import Layout from '../layout/Layout'
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
