7cddd210dc80839b2855a6e5e1f49fd30e4fb85d
Dima-kun <FaboritePonyville@gmail>
2023-07-10 09:35:14 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react";
-import { TextEditorProvider } from '@/Components/TextEditor2/context';
 
 export default function App({
   Component,
