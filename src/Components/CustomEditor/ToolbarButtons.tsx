4bea11bcf04bba97a8a764385245fa1e079cc305
Dima-kun <FaboritePonyville@gmail>
2023-07-14 17:32:29 +0300

    test new block draft js

--- src/Components/CustomEditor/ToolbarButtons.tsx
+++ src/Components/CustomEditor/ToolbarButtons.tsx
@@ -1,10 +1,12 @@
-import { isBold, isItalic, isUnderline, toggleBold, toggleItalic, toggleUnderline } from "contenido";
+import { isBold, isItalic, isUnderline, toggleBold, toggleItalic, toggleUnderline, toggleSub, isSub, toggleH1, isH1} from "contenido";
 import { EditorStateProps } from "./CustomEditor.props";
 
 const toolbarButtons = [
   { name: "Bold", handler: toggleBold, detector: isBold },
   { name: "Italic", handler: toggleItalic, detector: isItalic },
   { name: "Underline", handler: toggleUnderline, detector: isUnderline },
+  { name: 'Sub', handler: toggleSub, detector:isSub },
+  { name: 'h1', handler: toggleH1, detector:isH1 },
 ];
 
 const ToolbarButtons = ({editorState, setEditorState}:EditorStateProps) => {
