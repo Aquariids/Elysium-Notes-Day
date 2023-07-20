a2a0d6c490111e63f21fadc923243ed530af19de
Dima-kun <FaboritePonyville@gmail>
2023-07-20 18:27:37 +0300

    fix api

--- pages/api/getTitle.js
+++ pages/api/getTitle.js
@@ -1,11 +1,15 @@
 import { getTitleFromDatabase } from './auth/lib/dataFromDatabase';
 
 export default async function handler(req, res) {
-    try {
+ 
         const _id = req.query._id
-        const data = await getTitleFromDatabase(_id);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ message: error.message});
-    }
+
+        if(!_id) {
+            res.status(500).json("Такого заголовка нет");
+        } else {
+            const data = await getTitleFromDatabase(_id);
+            res.status(200).json(data);
+        }
+   
+
 }
