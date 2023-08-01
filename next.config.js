1290e0412f0b667fff355594ebc0b46f83f4ed4d
Dima-kun <FaboritePonyville@gmail>
2023-08-02 00:05:05 +0300

    update next pwa

--- next.config.js
+++ next.config.js
@@ -2,11 +2,26 @@
  * @type {import('next').NextConfig}
  */
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+	urlPattern: /^\/api\//,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-data-cache',
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 60 * 60 // 1 hour
+        },
+      },
+    },
+  ],
+  publicExcludes: ['!robots.txt', '!sitemap.xml']
   
 })
 
-module.exports =  withPWA({
+module.exports = withPWA({
   webpack(nextConfig) {
 	nextConfig.module.rules.push({
 			loader: '@svgr/webpack',
