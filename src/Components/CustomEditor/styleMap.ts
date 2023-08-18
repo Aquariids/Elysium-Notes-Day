5db1646e2d6d74573b3b1f9130724c89116e59cc
Dima-kun <FaboritePonyville@gmail>
2023-08-18 06:47:39 +0300

    fix

--- src/Components/CustomEditor/styleMap.ts
+++ src/Components/CustomEditor/styleMap.ts
@@ -2,6 +2,6 @@ import { initialStyleMap } from "contenido";
 export const styleMap = {
     ...initialStyleMap,
     HIGHLIGHTER: {
-      backgroundColor: "#b11f1f85",
+      backgroundColor: "rgb(248, 248, 175)",
     },
   };
