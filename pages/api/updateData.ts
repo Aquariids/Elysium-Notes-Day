982c029d9b20d06de909da092e289058f41f7523
Dima-kun <FaboritePonyville@gmail>
2023-08-26 09:16:55 +0300

    fix

--- pages/api/updateData.ts
+++ pages/api/updateData.ts
@@ -12,30 +12,21 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     const data = req.body;
     switch (action) {
       case update_action.editor:
-        await updateDataInDatabase(data); 
-        console.log(
-'1'
-        );
-              
+        await updateDataInDatabase(data);
         res.status(200).send("Data editor updated successfully");
         break;
       case update_action.editor_title:
-        console.log('2');
         await updateDataTitle(data);
         res.status(200).send("Data title updated successfully");
         break;
       case update_action.book_main_menu:
         await updateNoteBookMainMenu(data);
-        console.log(
-            '3'
-                    );
         res.status(200).send("Data bookMainMenu updated successfully");
         break;
       default:
         res.status(400).send("Invalid action");
         return;
     }
-
   } catch (error) {
     res.status(500).send("An error occurred while updating data");
   }
