7a0054e006a100a1e33fd7e4b3bbf8b23fc5ff68
Dima-kun <FaboritePonyville@gmail>
2023-08-28 10:14:40 +0300

    fix

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,24 +1,15 @@
 import s from "./Sidebar.module.scss";
 import { SidebarProps } from "./Sidebar.props";
 import cn from "classnames";
-import Login from "../../pages/signin";
-import ButtonCreateNewNotes from "@/Components/ButtonCreateNewNotes/ButtonCreateNewNotes";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../pages/api/auth/[...nextauth]";
-import { useSession } from "next-auth/react";
+import React from "react";
 import SideBarMenu from "@/Components/SidebarMenu/SidebarMenu";
 const Sidebar = ({ className,  ...props }: SidebarProps) => {
   
-  const { data }: any = props;
- const session = useSession();
 
   return (
     <div className={cn(className, s.sidebar)} {...props}>
 
-      <SideBarMenu data ={data}/>
+      <SideBarMenu/>
       
     </div>
   );
