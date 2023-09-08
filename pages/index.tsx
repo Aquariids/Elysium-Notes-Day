78558b64bd157be0d5a39e5f88e7c8165ae6eab9
Dima-kun <FaboritePonyville@gmail>
2023-09-09 01:17:32 +0300

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,34 @@ function Home({ data_editor, data_note_main_menu }: any) {
   const session = useSession();
   const userId = session.data?.user.userId;
   const email = session.data?.user.email;
+  const [sort, setSort] = useState<any>();
+
+
+  useEffect(() => {
+    const sort = localStorage.getItem("sorting") || "no-sorting";
+    setSort(sort);    
+  }, []);
+
+  function sortBody(body: any) {
+    try {
+      const sortBody = body.sort((a: any, b: any) => {
+        const dateA = Date.parse(a.date);
+        const dateB = Date.parse(b.date);
+  
+        if (sort === "dateUp") return dateB - dateA; // Сравниваем в обратном порядке для сортировки от новых к старым
+        if (sort === "dateDown") return dateA - dateB;
+        if(sort=== 'no-sorting')  return body;
+      });
+  
+      return sortBody;
+    }
+
+    catch (err) {
+      console.log(err);
+      
+    }
+
+  }
 
 
   const dataNoteBook = {
@@ -127,7 +155,7 @@ function Home({ data_editor, data_note_main_menu }: any) {
           </Link>
           </div>
           <div className={s.container}>
-            <List className={s.link} body={data_editor} />
+            <List className={s.link} body={sortBody(data_editor)} />
             <NewNotesMainMenu />
           </div>
         </div>
