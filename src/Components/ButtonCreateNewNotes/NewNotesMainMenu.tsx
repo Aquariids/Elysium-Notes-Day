64cac149d5001db14d3a08dfd8dba827da4a0c1a
Dima-kun <FaboritePonyville@gmail>
2023-09-09 17:14:42 +0300

    fix

--- src/Components/ButtonCreateNewNotes/NewNotesMainMenu.tsx
+++ src/Components/ButtonCreateNewNotes/NewNotesMainMenu.tsx
@@ -5,20 +5,27 @@ import { NOTES } from "../../../pages/api/paths";
 import s from "./ButtonCreateNewNotes.module.scss";
 import AddNotes from "./add_notes.svg";
 import { create_data } from "../../../pages/api/actios";
-
+import { DateTime } from 'luxon';
+import { Settings } from 'luxon';
+Settings.defaultLocale = 'ru';
+DateTime.local().setLocale('ru');
 const ButtonCreateNewNotes = () => {
   const { data: session } = useSession();
   // emptyRawContentState - пустой объект содержимого draft js. Превращаем его в JSON и отправляем в базу
   const router = useRouter();
   const create = async () => {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const userDate = DateTime.now().setZone(userTimeZone);
     const content = JSON.stringify(emptyRawContentState);
-
     const data = {
       userId: session?.user.userId,
       email: session?.user.email,
       body: content, // данные редактора
       title: "",
       block: false,
+      date:userDate.toJSDate(),
+      dateFull:userDate.toFormat("EEEE, d MMMM yyyy HH:mm"),
+      dateShort:userDate.toFormat("d MMMM").slice(0, 5) + '.',
     };
 
     try {
