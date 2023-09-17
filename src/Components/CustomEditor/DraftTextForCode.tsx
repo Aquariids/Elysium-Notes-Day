1abe2dfb5a59e454ac07d82a884cfbdfb96fbd4d
Dima-kun <FaboritePonyville@gmail>
2023-09-17 11:17:02 +0300

    fix

--- src/Components/CustomEditor/DraftTextForCode.tsx
+++ src/Components/CustomEditor/DraftTextForCode.tsx
@@ -3,8 +3,13 @@ import { useEffect, useRef, useState } from "react";
 const DraftTextForCode = ({editorState, setTest}:any) => {
       const refActiveMenu = useRef<HTMLDivElement>(null);
 
-      const handleOutsideClick = () => {
-            setTest(true);
+      const handleOutsideClick = (event:any) => {
+        if (
+          refActiveMenu.current &&
+          !refActiveMenu.current.contains(event.target)
+        ) {
+          setTest(true)
+        }
           };
         
           useEffect(() => {
