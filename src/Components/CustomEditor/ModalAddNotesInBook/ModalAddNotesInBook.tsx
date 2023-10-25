f71c85cbeedf71521d428adbe8f0ac9465bf2f56
Dima-kun <FaboritePonyville@gmail>
2023-10-25 10:36:46 +0300

    fix

--- src/Components/CustomEditor/ModalAddNotesInBook/ModalAddNotesInBook.tsx
+++ src/Components/CustomEditor/ModalAddNotesInBook/ModalAddNotesInBook.tsx
@@ -4,47 +4,43 @@ import s from "./ModalAddNotesInBook.module.scss";
 import cn from "classnames";
 import { useRouter } from "next/router";
 import Xmark from "./xmark.svg";
-import Done from './done.svg';
+import Done from "./done.svg";
 const ModalAddNotesInBook = ({
   active,
   setActive,
   currentNote,
   session,
-  
 }: any) => {
-
   const router = useRouter();
   const [currentIdPage, setCurrentIdPage] = useState<string>("");
   const [activeLink, setActiveLink] = useState<any>(false);
   const [allBooks, setAllBooks] = useState([]);
   const email = session?.user.email;
-  const userId =  session?.user.userId;
+  const userId = session?.user.userId;
 
   const getBook = useCallback(async () => {
     const resBook = await fetch(
       `/api/getData?action=${get_action.id_page_book}&userId=${userId}&email=${email}`
     );
     const databook = await resBook.json();
-    setAllBooks(databook)
-  },[allBooks])
+    setAllBooks(databook);
+  }, [allBooks]);
 
   useEffect(() => {
     getBook();
-  }, [router])
-  
-  function close () {
-    setActive(false)
+  }, [router]);
+
+  function close() {
+    setActive(false);
     setTimeout(() => {
-      setActiveLink('')
-    },1000)
+      setActiveLink("");
+    }, 1000);
   }
 
   const addIdPageForNote = async () => {
-    
-    
     const data = {
       email: email,
-      userId:userId,
+      userId: userId,
       _id: currentNote._id,
       idPage: currentIdPage,
     };
@@ -79,41 +75,58 @@ const ModalAddNotesInBook = ({
         </span>
         <div className={s.body__content}>
           <div className={s.books}>
-             {allBooks && allBooks.map((item: any, i:number) => {
-  
-            return (
-                <span
-                className={cn({
-                  [s.currentActiveBook]: activeLink._id === item._id,
-                  [s.activeBook]: currentNote.idPage === String(item.idPage) && !activeLink,
-                })}
-                  onClick={(e) => {
-                    setCurrentIdPage(String(item.idPage));
-                    setActiveLink(item)
-                  }}
-                  key={item._id}
-                >
-                  <div className={s.content_link}>
-                            <Done
-                              className={cn(s.hide, {
-                                [s.show]:
-                                  (activeLink._id  == item.idPage &&
-                                    !currentIdPage) ||
-                                  currentIdPage === String(item.idPage),
-                              })}
-                            />{" "}
-                            <span className={s.text}>{item.name}</span>
-                          </div>
-                </span>
-              );
-            })}
+            {allBooks && allBooks.length > 0 ? (
+              allBooks.map((item: any, i: number) => {
+                return (
+                  <span
+                    className={cn({
+                      [s.currentActiveBook]: activeLink._id === item._id,
+                      [s.activeBook]:
+                        currentNote.idPage === String(item.idPage) &&
+                        !activeLink,
+                    })}
+                    onClick={(e) => {
+                      setCurrentIdPage(String(item.idPage));
+                      setActiveLink(item);
+                    }}
+                    key={item._id}
+                  >
+                    <div className={s.content_link}>
+                      <Done
+                        className={cn(s.hide, {
+                          [s.show]:
+                            (activeLink._id === item._id && !currentIdPage) ||
+                            currentNote.idPage === String(item.idPage),
+                        })}
+                      />{" "}
+                      <span className={s.text}>{item.name}</span>
+                    </div>
+                  </span>
+                );
+              })
+            ) : (
+              <span>Здесь нет доступных книг</span>
+            )}
           </div>
           <div className={s.footer__buttons}>
-            <button className={s.btn} onClick={close}>Отмена</button>
-            <button className={s.btn} disabled={activeLink._id && String(activeLink.idPage) !== currentNote.idPage ? false : true} onClick={()=> {
-              addIdPageForNote()
-              setActive(false)
-            }}>Готово</button>
+            <button className={s.btn} onClick={close}>
+              Отмена
+            </button>
+            <button
+              className={s.btn}
+              disabled={
+                activeLink._id &&
+                String(activeLink.idPage) !== currentNote.idPage
+                  ? false
+                  : true
+              }
+              onClick={() => {
+                addIdPageForNote();
+                setActive(false);
+              }}
+            >
+              Готово
+            </button>
           </div>
         </div>
       </div>
