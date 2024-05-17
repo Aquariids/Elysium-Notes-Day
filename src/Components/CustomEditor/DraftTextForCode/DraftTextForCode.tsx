fd10c0640386cbdf63f5f424b84e0033a955bfc3
Dimasik <FavoritePonyville@gmail.com>
2024-05-17 19:38:53 +0400

    fix scss codeSandbox

--- src/Components/CustomEditor/DraftTextForCode/DraftTextForCode.tsx
+++ src/Components/CustomEditor/DraftTextForCode/DraftTextForCode.tsx
@@ -1,33 +1,32 @@
 import { EditorState } from "draft-js";
-import {SandpackProvider, SandpackLayout, SandpackCodeViewer} from "@codesandbox/sandpack-react";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeViewer,
+} from "@codesandbox/sandpack-react";
 import { aquaBlue } from "@codesandbox/sandpack-themes";
-import s from './DraftTextForCode.module.scss';
 interface props {
-  editorState:EditorState,
-  setShowToolbar: (value: boolean) => void
-  routerReclycle: boolean
+  editorState: EditorState;
+  setShowToolbar: (value: boolean) => void;
+  routerReclycle: boolean;
 }
 const DraftTextForCode = ({
   editorState,
   setShowToolbar,
   routerReclycle,
 }: props) => {
-
   const plainText = editorState.getCurrentContent().getPlainText();
 
   return (
-    <div className={s.container} onClick={()=> {
-      routerReclycle === false && setShowToolbar(true);
-    }} >
-      <SandpackProvider theme={aquaBlue} >
-        <SandpackLayout>
-          <SandpackCodeViewer code={ plainText}/>
-        </SandpackLayout>
-      </SandpackProvider>
-
-
-    </div>
-    
+    <SandpackProvider theme={aquaBlue}>
+      <SandpackLayout
+        onClick={() => {
+          routerReclycle === false && setShowToolbar(true);
+        }}
+      >
+        <SandpackCodeViewer code={plainText} />
+      </SandpackLayout>
+    </SandpackProvider>
   );
 };
 
