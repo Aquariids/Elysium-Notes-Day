cf4d273cbf34394c0e1ed9ea0944d2d162ba039d
Dimasik <FavoritePonyville@gmail.com>
2024-06-14 16:25:00 +0300

    fix/raw of search

--- src/Components/NotesList/NotesList.tsx
+++ src/Components/NotesList/NotesList.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
 import List from "./List";
-import Search from "../Search/Search";
 import Fuse from "fuse.js";
 const NotesList = ({
   dataClient,
@@ -11,31 +10,42 @@ const NotesList = ({
   loadingDelete,
 }: any) => {
  
-  const [notes, setNotes] = useState<{ _id: string; title: string; body: string }[]>([...dataClient]);
-  const [filteredNotes, setFilteredNotes] = useState<{ _id: string; title: string; body: string }[]>([]);
+  // const [notes, setNotes] = useState<{ title: string; body: string }[]>([...dataClient]);
+  const [filteredNotes, setFilteredNotes] = useState<{  title: string; body: string }[]>([]);
+  console.log("🚀 ~ filteredNotes:", filteredNotes)
   const [query, setQuery] = useState('');
 
   const fuse = useMemo(() => {
-    return new Fuse(notes, {
-      keys: ['title', 'body'], // Поиск по заголовку и содержимому
+    return new Fuse(dataClient, {
+      keys: ['title', 'body'], // Поиск по заголовку и содержимому body в текст надо будет превратить
       includeScore: true, // Включение рейтинга совпадений
       threshold: 0.3, // Чувствительность поиска (0.0 - точное совпадение, 1.0 - очень нечеткое)
     });
-  }, [notes]);
+  }, [dataClient]);
 
   useEffect(() => {
     if (query) {
       const results = fuse.search(query);
       setFilteredNotes(results.map(result => result.item));
     } else {
-      setFilteredNotes(notes);
+      setFilteredNotes(dataClient);
     }
 
     
   }, [query, fuse]);
 
   if (!dataClient) {
-    return <List body={dataServer} userId={userId} email={email} />;
+    return(
+    <>
+     <input
+        type="text"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        placeholder="Поиск заметок..."
+      />
+       <List body={dataServer} userId={userId} email={email} />;
+    </>
+    )
   } else {
     
     return (
